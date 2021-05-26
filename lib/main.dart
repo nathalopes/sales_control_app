f6eac8cb20d37954c1a422044d7cd6e8e4684154
Nathã Luca Pereira Lopes <natha.luca.p.lopes@outlook.com>
2021-05-25 23:32:56 -0300

    Taking MyApp widget back to main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
-import 'features/my_app.dart';
+import 'package:sales_control/features/pages/change_password_page.dart';
+import 'package:sales_control/features/pages/history_page.dart';
+import 'package:sales_control/features/pages/login_page.dart';
+import 'package:sales_control/features/pages/test_page.dart';
+import 'features/pages/home_page.dart';
+import 'features/pages/register_page.dart';
 
 void main() async {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/register': (context) => RegisterPage(),
+        '/history': (context) => HistoryPage(),
+        '/change_password': (context) => ChangePasswordPage(),
+        'teste': (context) => TestPage(),
+      },
+    );
+  }
 }
